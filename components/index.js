3ccbbe29c85670ffabc56b1b2aeea5e247c05245
austinjp <austin.plunkett@gmail.com>
2021-11-22 11:30:29 +0000

    Latest components

--- components/index.js
+++ components/index.js
@@ -3,18 +3,20 @@ import { View } from "./View"
 
 // FIXME TODO: Button, Grid, Image, StyleSheet, Switch
 import { FAB } from "./FAB"
+import { Ident } from "./Ident"
 import { Link } from "./Link"
 import { Modal } from "./Modal"
 import { Nav } from "./Nav"
 import { Text } from "./Text"
 import { TextInput } from "./TextInput"
-import { TextLink } from "./TextLink"
+// import { TextLink } from "./TextLink"
 import { Toolbar } from "./Toolbar"
 import { Swiper } from "./Swiper"
 
 const exp = {
   // Button: Button, Grid: Grid, Image: Image,
   FAB: FAB,
+  Ident: Ident,
   Link: Link,
   Modal: Modal,
   Nav: Nav,
@@ -23,7 +25,7 @@ const exp = {
   // Switch: Switch,
   Text: Text,
   TextInput: TextInput,
-  TextLink: TextLink,
+  // TextLink: TextLink,
   Toolbar: Toolbar,
   View: View,
 }
@@ -32,6 +34,7 @@ export default exp
 export {
   // Button, Grid, Image
   FAB,
+  Ident,
   Link,
   Modal,
   Nav,
@@ -40,7 +43,7 @@ export {
   // Switch,
   Text,
   TextInput,
-  TextLink,
+  // TextLink,
   Toolbar,
   View,
 }
