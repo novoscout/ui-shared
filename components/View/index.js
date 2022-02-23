b01ef1fdbeba4c256be56dca2aaa78bd765bddfb
austinjp <austin.plunkett@gmail.com>
2022-02-23 12:45:01 +0000

    Fancy destructuring/renaming
    instead of copying/deleting

--- components/View/index.js
+++ components/View/index.js
@@ -3,10 +3,8 @@ import { React, h } from "../../lib/react-preact"
 const View = (props) => {
   const elem = props.elem ? String(props.elem) : "div"
 
-  // Remove items that otherwise will end up in HTML.
-  let newProps = {...props}
-
-  delete newProps.elem
+  // Remove props items that otherwise will end up in HTML.
+  const { elem:{}, ...newProps } = props
 
   return React.createElement(elem, newProps)
 }
